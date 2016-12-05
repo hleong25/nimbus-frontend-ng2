8ddd798a343207ad0c5ca1c0e4c8b32100b8e20a
henry leong <hleong25+github@gmail.com>
2016-12-05 10:54:41 -0800

    cleanup and add shared files for home and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-  /*
-import {
-  DashboardComponent,
-  ContentTypeComponent,
-  SearchBoxComponent
-} from './dashboard/dashboard';
-
-import {
-  HomeModule,
-} from './home/home.module';
-  */
-
-import { HomeComponent } from './home/components/home.component';
-import { DashboardComponent } from './dashboard/components/dashboard.component';
+import { HomeComponent } from './home/home.shared';
+import { DashboardComponent } from './dashboard/dashboard.shared';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
 
-
-  //{ path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule' },
-  //{ path: 'dashboard', loadChildren: 'app/dashboard/search-box/search-box.module#SearchBoxModule' },
 ];
 
 @NgModule({

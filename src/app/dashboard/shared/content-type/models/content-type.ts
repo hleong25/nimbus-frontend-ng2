3a8c1ca38b6d46783b28a84c20c5068c7cfb1beb
henry leong <hleong25+github@gmail.com>
2016-12-11 09:04:40 -0800

    add cloud/content-type to dashboard/content-type

--- src/app/dashboard/shared/content-type/models/content-type.ts
+++ src/app/dashboard/shared/content-type/models/content-type.ts
@@ -1,4 +1,8 @@
+import { ContentType as CloudContentType } from '../../../../cloud/models/content-type';
+
 export class ContentType {
+  type: CloudContentType;
+
   title: string;
   icon: string;
 }
--- src/app/dashboard/shared/content-type/services/mock-content-types.ts
+++ src/app/dashboard/shared/content-type/services/mock-content-types.ts
@@ -1,8 +1,9 @@
+import { ContentType as CloudContentType } from '../../../../cloud/models/content-type';
 import { ContentType } from '../models/content-type';
 
 export const CONTENT_TYPES: ContentType[] = [
-    {title: 'documents', icon: 'fa-file-text-o'},
-    {title: 'videos', icon: 'fa-film'},
-    {title: 'music', icon: 'fa-music'},
-    {title: 'pictures', icon: 'fa-camera'},
+    {type: CloudContentType.Document, title: 'documents', icon: 'fa-file-text-o'},
+    {type: CloudContentType.Video, title: 'videos', icon: 'fa-film'},
+    {type: CloudContentType.Audio, title: 'music', icon: 'fa-music'},
+    {type: CloudContentType.Image, title: 'pictures', icon: 'fa-camera'},
 ];
